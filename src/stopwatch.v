0c332885521d041fd97a30a7a3305155b5027c9d
Manoranjan513 <manoranjanofmadurai@gmail.com>
2025-08-26 00:28:54 +0530

    Update stopwatch.v

--- src/stopwatch.v
+++ src/stopwatch.v
@@ -6,8 +6,6 @@ module stopwatch(
     wire clk_out;
     reg running;
 
-    clk_divider cd(.clk(clk), .rst(rst), .clk_out(clk_out));
-
     always @(posedge clk or posedge rst) begin
         if (rst)
             running <= 0;
